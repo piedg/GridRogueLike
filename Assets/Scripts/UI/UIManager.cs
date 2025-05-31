3da82cf11c0a5118b951d1b48b987d67d6bf65a8
Pietro <pietro.digiovanni.dev@gmail.com>
2025-06-01 01:08:31 +0200

    GameOver Screen + status text

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,14 +1,36 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 { 
     [SerializeField] Text hungryText;
-    [SerializeField]  Text healthText;
+    [SerializeField] Text healthText;
+    [SerializeField] Text statusText;
+    [SerializeField] GameObject gameOverPanel;
+
+    private void Start()
+    {
+        gameOverPanel.SetActive(false);
+    }
 
     private void Update()
     {
         hungryText.text = $"Hungry: {GameManager.Instance.GetPlayerHungry()}";
         healthText.text = $"Health: {GameManager.Instance.GetPlayerHealth()}";
+
+        if (GameManager.Instance.GetPlayerHungry() == 0)
+        {
+            statusText.text = "You are starving!";
+        }
+        else
+        {
+            statusText.text = "";
+        }
+
+        if (GameManager.Instance.GetPlayerHealth() == 0)
+        {
+            gameOverPanel.SetActive(true);
+        }
     }
 }
